4d8b2221d5a9795ba4566a63e790b0eea147a04e
Bhargava Naidu <bhargava@bootlabstech.com>
2025-07-08 22:55:38 +0530

    updated variable description 5.46.0

--- variables.tf
+++ variables.tf
@@ -1,67 +1,90 @@
-variable "region" {
-  description = "value"
-  type        = string
-}
 variable "bucket_name" {
-  description = "value"
+  description = "The name of the S3 bucket to be created"
   type        = string
 }
+
 variable "cloudfront_oai_comment" {
-  description = "value"
+  description = "A comment to identify the CloudFront Origin Access Identity (OAI)"
   type        = string
+  default     = "Test CloudFront OAI"
 }
+
 variable "s3_distribution_enabled" {
-  description = "value"
+  description = "Flag to enable or disable the CloudFront distribution for the S3 bucket"
   type        = bool
+  default     = true
 }
+
 variable "versioning_enabled" {
-  description = "value"
+  description = "S3 bucket versioning configuration: Enabled or Disabled"
   type        = string
-  default = "Disabled"
+  default     = "Enabled"
 }
+
 variable "block_public_acls" {
-  description = "value"
+  description = "Whether to block public access control lists (ACLs) for the S3 bucket"
   type        = bool
-  default = false
+  default     = true
 }
 
 variable "cache_allowed_methods" {
-  description = "value"
-  type        = list (string)
+  description = "List of HTTP methods that CloudFront caches responses for"
+  type        = list(string)
+  default     = ["GET", "HEAD", "OPTIONS"]
 }
+
 variable "cached_methods" {
-  description = "value"
-  type        = list (string)
+  description = "Subset of allowed methods that are cached by CloudFront"
+  type        = list(string)
+  default     = ["GET", "HEAD"]
 }
+
 variable "query_string" {
-  description = "value"
+  description = "Specifies whether CloudFront forwards query strings to the origin"
   type        = bool
+  default     = false
 }
+
 variable "cookies_forward" {
-  description = "value"
+  description = "Specifies how CloudFront handles cookies: none, whitelist, all"
   type        = string
+  default     = "none"
 }
+
 variable "viewer_protocol_policy" {
-  description = "value"
+  description = "Policy that determines whether viewers can use HTTP and/or HTTPS to access content"
   type        = string
 }
+
 variable "restriction_type" {
-  description = "value"
+  description = "Geo restriction type for CloudFront: none, whitelist, or blacklist"
   type        = string
 }
+
 variable "geo_locations" {
-  description = "value"
-  type        = list (string)
+  description = "List of country codes for geographic restriction in CloudFront"
+  type        = list(string)
+  default     = ["US", "CA", "IN"]
 }
+
 variable "cloudfront_default_certificate" {
-  description = "value"
+  description = "Whether to use the default CloudFront certificate (for HTTPS)"
   type        = bool
+  default     = true
 }
+
 variable "retain_on_delete" {
-  description = "value"
+  description = "Whether to retain the CloudFront distribution on Terraform destroy"
   type        = bool
+  default     = false
 }
- variable "role_arn" {
-   description = " The ARN of the IAM role"
-  type = string
+
+variable "region" {
+  description = "The AWS region in which resources will be created"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "The ARN of the IAM role to be assumed by Terraform"
+  type        = string
 }
